aee17d5a43070e84973cc82c89c8f727706dae9c
sirshelley <sirshelley@hotmail.co.uk>
2018-12-10 08:05:31 +0000

    Restructured, renamed some files and created addditional files for packaging as a python module

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name="DNCImproved",
+    version="0.1",
+    packages=find_packages(),
+     scripts=
+     [
+        'access.py.py',
+        'addressing.py',
+        'dnc.py',
+        'repeat_copy.py',
+        'trainRAndomDAta.py'
+        'util.py'
+     ],
+)
